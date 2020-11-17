f0c337d4eb29032268c709662a95f0dc11e59b3f
sunjin7725 <sunjin7725@gmail.com>
2020-11-17 22:34:45 +0900

    Updated solution2

--- day7/LV3/Solution.py
+++ day7/LV3/Solution.py
@@ -1,8 +1,8 @@
-# arr1 = [4, 3, 2, 3, 4]
-# s1 = 6
+arr1 = [4, 3, 2, 3, 4]
+s1 = 6
 
-# arr2 = [10, 5, 2, 3, 7, 5]
-# s2 = 10
+arr2 = [10, 5, 2, 3, 7, 5]
+s2 = 10
 
 def solution1(arr, s):
     inner = []
@@ -10,5 +10,13 @@ def solution1(arr, s):
         for j in inner:
             if s - j == i:
                 return [j ,i]
+        inner.append(i)
+    return None
+
+def solution2(arr, s):
+    inner = []
+    for i in arr1:
+        if s1 - i in inner:
+            return [s1 - i, i]
         inner.append(i)
     return None
