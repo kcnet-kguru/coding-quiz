9474a71728967ac7441419119f8bf2611154b6df
sunjin7725 <sunjin7725@gmail.com>
2020-12-13 23:23:16 +0900

    day25 lvl3 solved

--- day25/lv3/test.py
+++ day25/lv3/test.py
@@ -3,11 +3,9 @@ import Solution
 import datetime
 from copy import deepcopy
 
-
 test_case = [
-    (1, 250, [[1,1], [42,2500], [246, 84100]]),
-    (42, 250, [[42, 2500], [246, 84100]]),
-    (250, 500, [[287, 84100]])
+    ('sTreSS', 'T'),
+    ('moonmen', 'e')
             ]
 
 
@@ -17,7 +15,7 @@ class Test(unittest.TestCase):
         copy_case = deepcopy(test_case)
         for idx, case in enumerate(copy_case):
             start_time = datetime.datetime.now()
-            self.assertEqual(Solution.listSquared(case[0], case[1]), case[2])
+            self.assertEqual(Solution.first_non_repeating_letter(case[0]), case[1])
             end_time = datetime.datetime.now()
             print(f"TestCase #{idx} : {end_time - start_time}")
 if __name__ == '__main__':
